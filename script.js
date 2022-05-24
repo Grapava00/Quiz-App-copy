b2cd8b6439da6a7a4ff1ead3d475323ae2795bee
Giorgi Rapava <85571784+Grapava00@users.noreply.github.com>
2022-05-24 13:42:34 +0400

    add 2

---
+++ script.js
@@ -0,0 +1,33 @@
+const startButton=document.getElementById("start-btn");
+const questionContainerElement=document.getElementById('question-container')
+
+
+startButton.addEventListener('click', startGame)
+
+
+
+function startGame(){
+    console.log('started');
+    startButton.classList.add("hide")
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+
+}
+
+function setNextQuestion(){
+
+}
+
+function selectAnswer(){
+
+}
+
+const questions= [
+    {
+        question:'what is 2+2',
+        answers : [
+            {text : '4', correct:true},
+            {text : '22', correct: false},
+        ]
+    }
+]
